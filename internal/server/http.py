c74ce3276ef48e17ff181955087d6327224d679c
macochen <chenjingmaco@gmail.com>
2025-04-04 15:29:18 +0800

    增加日志模块

--- internal/server/http.py
+++ internal/server/http.py
@@ -5,9 +5,7 @@
 @Author : chenjingmaco@gmail.com
 @File   : http.py
 """
-
-
-
+import logging
 import os
 
 from flask import Flask
@@ -16,6 +14,7 @@ from flask_migrate import Migrate
 
 from config import LLmConfig
 from internal.router import Router
+from internal.extension import logging_extension
 from internal.exception import CustomException
 from pkg.response import json, Response, HttpCode
 from pkg.sql import SQLAlchemy
@@ -43,9 +42,15 @@ class Http(Flask):
         self.register_error_handler(Exception, self._register_error_handler)
 
         # 4.初始化flask扩展
+        # 初始化数据库
         db.init_app(self)
+
         # 初始化数据库迁移的目录
         migrate.init_app(self, db, directory="internal/migration")
+
+        # 初始化日志记录器
+        logging_extension.init_app(self)
+
         # # 如果表不存在，则创建；已经使用了migrate做版本管理这里全部注释
         # with self.app_context():
         #     _ = App() # 确保能够检索到这个App model, 就是说如果未使用到App这个类是不会去创建
@@ -66,7 +71,10 @@ class Http(Flask):
 
     def _register_error_handler(self, error: Exception):
 
-        # 1.判断异常信息是不是我们的自定义异常，如果是则可以提取message和code等信息
+        # 1.日志记录异常信息
+        logging.error("An error occurred: %s", error, exc_info=True)
+
+        # 2.判断异常信息是不是我们的自定义异常，如果是则可以提取message和code等信息
         if isinstance(error, CustomException):
             return json(Response(
                 code=error.code,
@@ -74,7 +82,7 @@ class Http(Flask):
                 data=error.data if error.data is not None else {},
             ))
 
-        # 2.如果不是我们自定义的异常，则有可能是程序、数据库等抛出的异常，也可以提取信息，设置为FAIL状态码
+        # 3.如果不是我们自定义的异常，则有可能是程序、数据库等抛出的异常，也可以提取信息，设置为FAIL状态码
         if self.debug or os.getenv("FLASK_ENV") == "development":
             raise error
         else:
